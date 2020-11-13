5059717032d74b96ebed16846431b18287d99f36
Glory Agatevure <gloryagatevure@glorys-macbook-pro.local>
2020-11-13 13:05:27 +0100

    initial commit

---
+++ app/src/main/java/com/example/signuplogin/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.signuplogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnLogout;
+    private Session session;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        session = new Session(this);
+        if(!session.loggedin()){
+            logout();
+        }
+        btnLogout = (Button)findViewById(R.id.btnLogout);
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logout();
+            }
+        });
+    }
+
+    private void logout(){
+        session.setLoggedin(false);
+        finish();
+        startActivity(new Intent(MainActivity.this,LoginActivity.class));
+    }
+}
